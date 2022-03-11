0d5b45ed7d5086ece23ad3f9592fa522635e7699
lipollis <aline.pollis@hotmail.com>
2022-03-11 12:06:02 -0300

    MovieCard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 // import { library } from '@fortawesome/fontawesome-svg-core'
 // import { fab } from '@fortawesome/free-brands-svg-icons'
 // import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -5,11 +5,11 @@ function Pagination() {
     return (
         <div className="dsmovie-pagination-container">
             <div className="dsmovie-pagination-box">
-                <button className="dsmovie-pagination-button" disabled={true} >
+                <button className="dsmovie-pagination-button" aria-label="Arrow" disabled={true} >
                     <Arrow />
                 </button>
                 <p>{`${1} de ${3}`}</p>
-                <button className="dsmovie-pagination-button" disabled={false} >
+                <button className="dsmovie-pagination-button" aria-label="Arrow" disabled={false} >
                     <Arrow className="dsmovie-flip-horizontal" />
                 </button>
             </div>
