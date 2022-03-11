3c9368d8ca1318728da4c4c6ba681f73f01a9b29
lipollis <aline.pollis@hotmail.com>
2022-03-11 09:19:59 -0300

    Projeto criado

---
+++ backend/src/main/java/com/devsuperior/dsmoviesdd7/DsmovieSdd7Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmoviesdd7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieSdd7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieSdd7Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmoviesdd7/DsmovieSdd7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmoviesdd7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieSdd7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
